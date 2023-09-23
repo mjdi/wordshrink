c1fd710f2199d0d9af45dd3b5c443ef2a6be49f8
Marcus Diemand <mjdiemand@gmail.com>
2023-09-23 04:38:33 -0400

    shorthand hash algo, connected to engine.py, |thg| -> thought

--- engine.py
+++ engine.py
@@ -1,20 +1,36 @@
-import sys
+import os
+import pathlib
 import keyboard
 from enum import Enum
+import json
+
+import shorthand.generate as gn
+
+exec_dir = os.path.dirname(os.path.realpath(__file__))
+shorthands_filename = gn.shorthands_en_50k_filename # gn.shorthands_en_full_filename 
+shorthands_filepath = pathlib.Path(exec_dir) / 'shorthand' / shorthands_filename
 
 # constants
 class Prog_State(Enum):
     DEF = 1, # default
     SHD = 2, # shorthand
 
-_3x5_keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+_3x10_keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
              'p','q','r','s','t','u','v','w','x','y','z',',','.',';',"'"]
+
+### for qwerty the semi-colon key shares the same column as p and forward slash
+### so we might need to make concessions for that
+
+### for colemak dh we make the comma key correspond to the 'o' key
+
+PREFIX_DELIM_KEY = ";" # types on press & release to delimit prefix|root & root|suffix respectively
+SUFFIX_DELIM_KEY = "'" # types on press & release to delimit root|suffix & prefix|root respectively
 
 # globals
 prog_state = Prog_State.DEF
 space_held = False
-_3x5_keys_held = [0] * len(_3x5_keys)
-_3x5_key_sequence = [] # list of key names that have been pressed in order
+_3x10_keys_held = [0] * len(_3x10_keys)
+_3x10_key_sequence = [] # list of key names that have been pressed in order
 
 
 #{'alt', 'alt gr', 'ctrl', 'left alt', 'left ctrl', 'left shift', 
@@ -25,9 +41,45 @@ modifiers = list(keyboard.all_modifiers)
 modifiers_held = [0] * len(modifiers)
 
 # functions
-def handle_shorthand(shorthand):
-    print('_'.join(shorthand))
-    return
+def get_handle_shorthand_fn():
+
+    with open(shorthands_filepath, 'r') as f:
+        shorthand_to_words_dict = json.load(f)
+
+    def handle_shorthand(_3x10_key_sequence):
+        seq = "".join(_3x10_key_sequence)
+
+        shorthand = ""
+    
+        if PREFIX_DELIM_KEY not in seq and SUFFIX_DELIM_KEY not in seq:
+            # root word only with no prefixes or suffixes
+            shorthand = gn.SH_DELIM + seq + gn.SH_DELIM
+        else:
+            # prefix|root|suffix, but may be missing one of prefix or suffix, thus a delimiter is missing
+            shorthand = seq.replace(PREFIX_DELIM_KEY, gn.SH_DELIM).replace(SUFFIX_DELIM_KEY, gn.SH_DELIM)
+
+            if shorthand.count(gn.SH_DELIM) == 1:
+                # add delimiter to whichever side is missing it
+                if PREFIX_DELIM_KEY not in seq:
+                    shorthand = gn.SH_DELIM + shorthand
+                else:
+                    shorthand = shorthand + gn.SH_DELIM
+
+        if shorthand in shorthand_to_words_dict:
+            word = list(shorthand_to_words_dict[shorthand][0].keys())[0] # get first word, most common
+            print(f'Word found for shorthand: {shorthand} -> {word}') 
+            # TODO: keep track of previously typed spaces and use that to determine whether to add a space
+            keyboard.write(' ' + word + ' ')
+        else:
+            print(f'No word found for shorthand: {shorthand}')
+ 
+        return
+    
+    return handle_shorthand
+
+
+handle_shorthand_fn = get_handle_shorthand_fn()
+
 
 def create_modifiers_string():
     modifier_string = ''
@@ -40,8 +92,8 @@ def on_key(event):
     try: 
         global prog_state
         global space_held
-        global _3x5_keys_held
-        global _3x5_key_sequence
+        global _3x10_keys_held
+        global _3x10_key_sequence
         global modifiers_held
 
         # guard clauses to handle Space functionality
@@ -58,29 +110,31 @@ def on_key(event):
             keyboard.press_and_release('space') # input a single space
             space_held = False
 
-            if sum(_3x5_keys_held) > 0: # still some alphas held
-                for key in _3x5_key_sequence:
+            if sum(_3x10_keys_held) > 0: # still some alphas held
+                for key in _3x10_key_sequence:
                     keyboard.press_and_release(key) # type the sequecne
                     
             # reset
-            _3x5_key_sequence.clear()
-            keys_held = [0] * len(_3x5_keys_held)
+            _3x10_key_sequence.clear()
+            keys_held = [0] * len(_3x10_keys_held)
 
             prog_state = Prog_State.DEF
             return
 
-        # guard clause to handle 3x5 keys
-        if event.name in _3x5_keys:
+        # guard clause to handle 3x10 keys
+        if event.name in _3x10_keys:
             if prog_state == Prog_State.SHD:
                 if event.event_type == 'down': # on intial down press
-                    if _3x5_keys_held[_3x5_keys.index(event.name)] == 0:
-                        _3x5_key_sequence.append(event.name)
-                    _3x5_keys_held[_3x5_keys.index(event.name)] = 1 # ignore holding
+                    if _3x10_keys_held[_3x10_keys.index(event.name)] == 0:
+                        _3x10_key_sequence.append(event.name)
+                    _3x10_keys_held[_3x10_keys.index(event.name)] = 1 # ignore holding
                 else: # on release
-                    _3x5_keys_held[_3x5_keys.index(event.name)] = False
-                    if sum(_3x5_keys_held) == 0: # only space is held
-                        handle_shorthand(_3x5_key_sequence)
-                        _3x5_key_sequence.clear()
+                    if event.name == PREFIX_DELIM_KEY or event.name == SUFFIX_DELIM_KEY:
+                        _3x10_key_sequence.append(event.name) # add delimiter on release also
+                    _3x10_keys_held[_3x10_keys.index(event.name)] = False
+                    if sum(_3x10_keys_held) == 0: # only space is held
+                        handle_shorthand_fn(_3x10_key_sequence)
+                        _3x10_key_sequence.clear()
             else: # prog_state == Prog_State.DEF
                 if event.event_type == 'down':  
                     keyboard.send(create_modifiers_string()+event.name, event.event_type)
@@ -101,6 +155,7 @@ def on_key(event):
 
 
     except ValueError as error:
+        import sys
         print(error)
         sys.exit(0)
 
--- shorthand/generate.py
+++ shorthand/generate.py
@@ -2,17 +2,46 @@ import os
 import pandas as pd
 import pathlib 
 import json
+from enum import Enum
+
+class WORDLIST(Enum):
+    FULL = 0
+    TOP_FIFTY_THOUSAND = 1
+
+MIN_WORD_LEN = 7
+MIN_FREQ = 100 # accomodated is freq 160
+MIN_ROOT_WORD_LEN = 3 # idk 
+LONGEST_PREFIX_LEN = 6 # contra
+LONGEST_SUFFIX_LEN = 7 # ability
+
+VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
+ALLOWED_SHORTHAND_CHARACTERS = "abcdefghijklmnopqrstuvwxyz,.';"
+SH_DELIM = '|'
+
+# TODO: make this a command line arg
+
+# change this to en_full_filename if you want to use the full word list or the top 50k
+WORDLIST_TO_USE = WORDLIST.FULL
+#WORDLIST_TO_USE = WORDLIST.TOP_FIFTY_THOUSAND
 
 exec_dir = os.path.dirname(os.path.realpath(__file__))
 
-words_filepath = pathlib.Path(exec_dir).parent / 'words' / '2018' / 'en_50k.txt' 
-letters_to_prefixes_suffixes_filepath = \
-    pathlib.Path(exec_dir) / '_3x10_prefix_suffix.json'
+letters_to_prefixes_suffixes_filepath = pathlib.Path(exec_dir) / '_3x10_prefix_suffix.json'
+en_full_filename = 'en_full.txt'
+en_50k_filename = 'en_50k.txt'
+shorthands_en_full_filename = 'shorthands_en_full.json'
+shorthands_en_50k_filename ='shorthands_en_50k.json'
 
-shorthands_filepath = pathlib.Path(exec_dir).parent / 'shorthands.json'
-
-MIN_WORD_LEN = 7
-MIN_FREQ = 1000
+words_filepath = pathlib.Path(exec_dir).parent / 'words' / '2018'
+shorthands_filepath = pathlib.Path(exec_dir)
+if WORDLIST_TO_USE == WORDLIST.FULL:
+    words_filepath = words_filepath / en_full_filename
+    shorthands_filepath = shorthands_filepath / shorthands_en_full_filename 
+elif WORDLIST_TO_USE == WORDLIST.TOP_FIFTY_THOUSAND:
+    words_filepath = words_filepath / en_50k_filename
+    shorthands_filepath = shorthands_filepath / shorthands_en_50k_filename 
+else:
+    raise Exception("Invalid wordlist to use")
 
 WORD_KEY = 'word'
 PREFIXES_KEY = 'prefixes'
@@ -20,59 +49,97 @@ ROOT_WORD_KEY = 'root_word'
 SUFFIXES_KEY = 'suffixes'
 SHORTHAND_KEY = 'shorthand'
 
-VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
-SH_DELIM = '|'
-
 
 # closure to get the gen_algo_fn
 def get_gen_algo_fn():
 
     with open(letters_to_prefixes_suffixes_filepath, 'r') as f:
-        letters = json.load(f)
+        letter_to_presufs = json.load(f)
 
     def gen_algo_fn(word):
-        prefixes = []
-        prefixes_shorthand = []
-        suffixes = []
-        suffixes_shorthand = []
-        root_word = word 
+        found_prefixes = []
+        found_prefixes_shorthand = []
+        found_suffixes = []
+        found_suffixes_shorthand = []
+        original_word = word 
 
         # search for prefixes
-        for i in range(len(word)):
-            if word[i] in letters:
-                for prefix in letters[word[i]]["prefixes"]:
-                    if word.startswith(prefix):
-                        prefixes.append(prefix)
-                        prefixes_shorthand.append(word[i]) # append letter
-                        root_word = root_word[len(prefix):]
-                        break
+        no_more_prefixes = False
+        while not no_more_prefixes and len(word) >= MIN_ROOT_WORD_LEN:
+            no_more_prefixes = True # assume no more prefixes
+
+            # find all possible prefixes and sort them by length descending
+            possible_prefix_letters = set(word[:LONGEST_PREFIX_LEN])
+            possible_prefixes = [] # list of prefixes to search for in word
+            prefix_to_letter = {} # backreference to letter
+
+            for letter in possible_prefix_letters:
+                letter_prefixes = letter_to_presufs[letter][PREFIXES_KEY]
+
+                if letter_prefixes is None:
+                    continue
+
+                for prefix in letter_prefixes:
+                    prefix_to_letter[prefix] = letter
+                    possible_prefixes.append(prefix)
+
+            possible_prefixes.sort(key=lambda x: len(x), reverse=True) # sort by length descending
+
+            for prefix in possible_prefixes:
+                if word.startswith(prefix): # found prefix
+                    found_prefixes.append(prefix)
+                    found_prefixes_shorthand.append(prefix_to_letter[prefix]) # append letter
+                    word = word[len(prefix):] # remove prefix from word
+                    no_more_prefixes = False
+                    break
+
 
         # search for suffixes
-        for i in range(len(word)-1, -1, -1):
-            if word[i] in letters:
-                for suffix in letters[word[i]]["suffixes"]:
-                    if word.endswith(suffix):
-                        suffixes.append(suffix)
-                        suffixes_shorthand.append(word[i]) # append letter
-                        root_word = root_word[:-len(suffix)]
-                        break
+        no_more_suffixes = False
+        while not no_more_suffixes and len(word) >= MIN_ROOT_WORD_LEN:
+            no_more_suffixes = True # assume no more suffixes
 
-        # remove vowels from root word
-        root_word_with_vowels = root_word
-        root_word = ''.join([c for c in root_word if c not in VOWELS])
+            # find all possible suffixes and sort them by length descending
+            possible_suffix_letters = set(word[-LONGEST_SUFFIX_LEN:])
+            possible_suffixes = []
+            suffix_to_letter = {}
 
-        # take only the first 3 letters of the root word
-        shorthand = ''.join(prefixes_shorthand) + SH_DELIM + \
-                                  root_word[:3] + SH_DELIM + \
-                    ''.join(suffixes_shorthand)
+            for letter in possible_suffix_letters:
+                letter_suffixes = letter_to_presufs[letter][SUFFIXES_KEY]
+
+                if letter_suffixes is None:
+                    continue
+
+                for suffix in letter_suffixes:
+                    suffix_to_letter[suffix] = letter
+                    possible_suffixes.append(suffix) # to be reversed later
+
+            possible_suffixes.sort(key=lambda x: len(x), reverse=True) # sort by length descending
+
+            for suffix in possible_suffixes:
+                if word.endswith(suffix): # found suffix
+                    found_suffixes.append(suffix)
+                    found_suffixes_shorthand.append(suffix_to_letter[suffix]) # to be reversed later
+                    word = word[:-len(suffix)] # remove suffix from word
+                    no_more_suffixes = False
+                    break
+
+        # remove vowels from root word and take the first 3 letters
+        root_word_with_vowels = word # the remaining word is the root word, assuming the above algo is correct
+        root_word_shorthand = ''.join([c for c in root_word_with_vowels if c not in VOWELS])[:3]
+
+        # construct the shorthand
+        shorthand = ''.join(found_prefixes_shorthand) + SH_DELIM + \
+                                  root_word_shorthand + SH_DELIM + \
+                    ''.join(found_suffixes_shorthand[::-1])
 
         return  { 
-            word : 
+            original_word : 
             {  
                 SHORTHAND_KEY: shorthand, 
-                PREFIXES_KEY: prefixes,
+                PREFIXES_KEY: found_prefixes,
                 ROOT_WORD_KEY: root_word_with_vowels,
-                SUFFIXES_KEY: suffixes,
+                SUFFIXES_KEY: found_suffixes[::-1],
             }
         }
 
@@ -92,6 +159,8 @@ def main():
 
     shorthands_dict = {}
 
+    number_of_min_freq_skipped = 0
+ 
     for index, row in df.iterrows():
         word, freq, length = row['word'], int(row['freq']), row['length']
 
@@ -101,8 +170,20 @@ def main():
 
         if length < MIN_WORD_LEN or freq < MIN_FREQ:
             print(f"Skipping {word} with length {length} and freq {freq}")
+            if freq < MIN_FREQ:
+                number_of_min_freq_skipped += 1
+            if number_of_min_freq_skipped >= MIN_FREQ:
+                print("Stopping because we've skipped 100 words with freq < {MIN_FREQ}")
+                break
+
             continue
         
+        if not all(c in ALLOWED_SHORTHAND_CHARACTERS for c in word):
+            print(f"\tSkipping {word} with invalid characters")
+            continue
+        
+        number_of_min_freq_skipped = 0
+
         sh_dict = gen_algo_fn(word)
         shorthand = sh_dict[word][SHORTHAND_KEY]
 
